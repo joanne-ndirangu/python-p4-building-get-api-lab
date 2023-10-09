f895e15cb3854795e4a6adf5281e832b81d030b2
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-10-09 11:45:05 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -6,33 +6,61 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+@app.route("/")
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route("/bakeries", methods = ['GET'])
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+    return jsonify(bakery_list)
+
+
+@app.route('/bakeries/<int:id>', methods=['GET'])
+def get_bakery_by_id(id):
+    query = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = {
+            'id': query.id,
+            'name': query.name,
+            'created_at': query.created_at
+        }
+    response = make_response(jsonify(bakery_dict), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    query = BakedGood.query.order_by((BakedGood.price).desc())
+    listofbakedgoods = []
+    for bakedgood in query:
+        dict = {
+            'id': bakedgood.id,
+            'name': bakedgood.name,
+            'price': bakedgood.price,
+            'created_at': bakedgood.created_at
+            }
+        listofbakedgoods.append(dict)
+    return listofbakedgoods
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    query = BakedGood.query.order_by((BakedGood.price).desc()).first()
+    dict = {
+            'id': query.id,
+            'name': query.name,
+            'price': query.price,
+            'created_at': query.created_at
+            }
+    return make_response(dict, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,19 +2,31 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ("-baked_goods.bakery",)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref="bakery")
 
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
+
+    serialize_rules = ("-bakeries.baked_goods",)
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String, unique=True)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"<Baked_good ({self.name}) >"
